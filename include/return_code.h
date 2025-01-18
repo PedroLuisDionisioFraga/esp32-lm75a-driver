f71dd67c6a786b8238798946fff8335202febcd7
PedroLuisDionisioFraga <pedrodfraga@hotmail.com>
2025-01-18 01:58:00 -0300

    ✨ feat(lm75a_status_t): Add new return codes

--- include/return_code.h
+++ include/return_code.h
@@ -24,10 +24,15 @@ typedef enum
   LM75A_ERR_I2C_DEV_ADD,    ///< Failed to add LM75A device to the I2C bus
   LM75A_ERR_I2C_WRITE,      ///< I2C write operation failed
   LM75A_ERR_I2C_READ,       ///< I2C read operation failed
+  LM75A_ERR_DEV_HANDLE,     ///< Device handle is NULL
+  LM75A_ERR_READ_TEMP,      ///< Failed to read temperature
   LM75A_ERR_INVALID_PARAM,  ///< Invalid parameter passed
   LM75A_ERR_GPIO_CONFIG,    ///< GPIO configuration failed
   LM75A_ERR_ISR_INSTALL,    ///< ISR installation failed
+  LM75A_ERR_NO_MEM,         ///< Memory allocation failed
   LM75A_ERR_UNKNOWN         ///< Unknown error
 } lm75a_status_t;
+
+char *lm75a_status_to_string(lm75a_status_t status);
 
 #endif  // RETURN_CODE_H
